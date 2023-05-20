32ec85343b1ec7630975aff321e72eeed05ec909
rtwa-mn <134112075+rtwa-mn@users.noreply.github.com>
2023-05-20 15:37:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ function isInlinePDFSupported() {
   var element = document.createElement('object');
   return (typeof element.canPlayType === 'function' && element.canPlayType('application/pdf') !== '');
 }
+
+(function() {
+  'use strict';
+
+  if (isInlinePDFSupported()) {
+    var objectElement = document.createElement('object');
+    objectElement.setAttribute('data', 'RTWA-MN Scholarship 2023.pdf');
+    objectElement.setAttribute('type', 'application/pdf');
+    objectElement.setAttribute('width', '100%');
+    objectElement.setAttribute('height', '1000px');
+
+    document.getElementById('pdf-container').appendChild(objectElement);
+  } else {
+    var iframeElement = document.createElement('iframe');
+    iframeElement.setAttribute('src', 'your_pdf_file.pdf');
+    iframeElement.setAttribute('width', '100%');
+    iframeElement.setAttribute('height', '1000px');
+
+    document.getElementById('pdf-container').appendChild(iframeElement);
+  }
+})();
